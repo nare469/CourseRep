e35a4e6d17299501f714b71a1d8fdc44c0e485da
nare469 <nare469@gmail.com>
2014-11-01 21:06:21 -0400

    Models done. REST Set up.

--- CourseRep/CourseRep/urls.py
+++ CourseRep/CourseRep/urls.py
@@ -1,6 +1,78 @@
 from django.conf.urls import include, url
 from django.contrib import admin
+from courses.models import User, Course, Topic, Resource, Subscription, Vote
+from rest_framework import routers, serializers, viewsets
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ('username', 'first_name', 'last_name', 'password')
+
+class CourseSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Course
+        fields = ('code', 'name', 'description', 'users')
+        read_only_fields = ('code', 'name', 'description', 'users')
+
+class TopicSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Topic
+        fields = ('name', 'course')
+        read_only_fields = ('name', 'course')
+
+class ResourceSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Resource
+        fields = ('content', 'points', 'viewcount', 'author', 'topic')
+
+class SubscriptionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = ('user', 'course')
+
+class VoteSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Vote
+        fields = ('voter', 'resource', 'value')
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class CourseViewSet(viewsets.ModelViewSet):
+    queryset = Course.objects.all()
+    serializer_class = CourseSerializer
+
+class TopicViewSet(viewsets.ModelViewSet):
+    queryset = Topic.objects.all()
+    serializer_class = TopicSerializer
+
+class ResourceViewSet(viewsets.ModelViewSet):
+    queryset = Resource.objects.all()
+    serializer_class = ResourceSerializer
+
+class SubscriptionViewSet(viewsets.ModelViewSet):
+    queryset = Subscription.objects.all()
+    serializer_class = SubscriptionSerializer
+
+class VoteViewSet(viewsets.ModelViewSet):
+    queryset = Vote.objects.all()
+    serializer_class = VoteSerializer
+
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+router.register(r'topic', TopicViewSet)
+router.register(r'resource', ResourceViewSet)
+router.register(r'subscription', SubscriptionViewSet)
+router.register(r'vote', VoteViewSet)
+router.register(r'course', CourseViewSet)
+
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
+     url(r'^', include(router.urls)),
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
--- CourseRep/courses/admin.py
+++ CourseRep/courses/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from courses.models import User, Course, Subscription, Resource, Vote, Topic
 
-# Register your models here.
+admin.site.register(User)
+admin.site.register(Course)
+admin.site.register(Subscription)
+admin.site.register(Resource)
+admin.site.register(Vote)
+admin.site.register(Topic)
